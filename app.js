0418c00ebca283ed314bdc304e6781d20a37ec9d
Cirlan Andrei <andreynho2006@gmail.com>
2018-07-01 07:27:20 +0000

    make a search file to use a form to search for a term, make the result page a ul of film with name and id and use the seach from the form to search movies from the API

--- app.js
+++ app.js
@@ -3,14 +3,23 @@ var app = express();
 var request = require("request");
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res) {
+    res.render("search");
+})
+
 app.get("/results", function(req, res) {
-    request('https://api.themoviedb.org/3/search/company?page=1&query=california&api_key=7b50c3a83ecee9e0612aadff4bb3b634', function(error, respond, body) {
+    console.log(req.query.search);
+    var searchTerm = req.query.search;
+    //console.log(searchTerm);
+    request('https://api.themoviedb.org/3/search/company?page=1&query="'+ searchTerm +'"a&api_key=7b50c3a83ecee9e0612aadff4bb3b634', function(error, respond, body) {
     if(!error && res.statusCode == 200) {
         var data = JSON.parse(body);
         res.render("results", {data:data});
         }
     });
 });
+
+
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Movie app has started");
