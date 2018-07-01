d1641f31153f4a9603e62b11728f55958b2684ba
Cirlan Andrei <andreynho2006@gmail.com>
2018-07-01 06:58:29 +0000

    make a views folder with a result file to show the results, and make a request to the themoviedb API to search for a word in all the movies, parse the result and send it to the result page

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 var express = require("express");
 var app = express();
 var request = require("request");
-
+app.set("view engine", "ejs");
 
 app.get("/results", function(req, res) {
-     request('https://api.themoviedb.org/3/search/company?page=1&query=california&api_key=7b50c3a83ecee9e0612aadff4bb3b634', function(error, respond, body) {
-     if(!error && res.statusCode == 200) {
-         var parsebody = JSON.parse(body);
-        res.send(parsebody.results[0].name);
+    request('https://api.themoviedb.org/3/search/company?page=1&query=california&api_key=7b50c3a83ecee9e0612aadff4bb3b634', function(error, respond, body) {
+    if(!error && res.statusCode == 200) {
+        var data = JSON.parse(body);
+        res.render("results", {data:data});
         }
     });
-
-})
+});
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Movie app has started");
